6a661689f90c371b5010053b63089622c0d500fc
Uriel Salazar <usalazar1@student.rccd.edu>
2019-04-19 13:04:35 -0500

    2 scroll lists; final

---
+++ SyrusSUITS/Assets/keepPressed.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonToggler : MonoBehaviour
+{
+    public Button button;
+
+    public void ToggleButton()
+    {
+        // toggle the button image
+        button.targetGraphic.enabled = !button.targetGraphic.enabled;
+        // toggle the button's click functionality
+        button.interactable = !button.interactable;
+    }
+}
